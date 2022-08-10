53b23ca0a86bb2d5ebdf4f77e49f38328513b722
tatu.miinalainen@gmail.com <tatu.miinalainen@gmail.com>
2022-08-10 11:56:58 +0300

    Äänestksen poisto valmiiksi.

--- toiminta.js
+++ toiminta.js
@@ -9,7 +9,7 @@ var lopeta = false
 
 function Uudenluominen(divnimi, tyyppi, kysymys, vastaus){
     var uusidiv = document.createElement('div')
-    uusidiv.setAttribute("class", "form" + formCount)
+    uusidiv.setAttribute("id", "form" + formCount)
     var kysy = document.getElementById(kysymys).value
     var vaihtoehto = document.getElementById(vastaus).value
 
@@ -35,12 +35,13 @@ function Uudenluominen(divnimi, tyyppi, kysymys, vastaus){
         document.getElementById(kysymys).value = "";
     }
     else if(i < 6){
+        console.log(lopeta)
         if(lopeta == true){
             i = i - i + 6;
         }
         switch(tyyppi){
             case 'radio':
-                uusidiv.innerHTML = "<input type='radio' name='myRadioButtons>[]'>" + " " + vaihtoehto;
+                uusidiv.innerHTML = "<input type='radio' name='myRadioButtons[]'>" + " " + vaihtoehto;
                 break;
         }
         document.getElementById(divnimi).appendChild(uusidiv);
@@ -53,17 +54,21 @@ function Uudenluominen(divnimi, tyyppi, kysymys, vastaus){
         i++
     }
     else if(i == 8){
-        console.log(i)
-        uusidiv.innerHTML = '<button id="aanestys" type="submit">Submit</button>'
+        uusidiv.innerHTML = '<button id="aanestys" type="button" onclick="ClearAndVote()">Submit</button>'
+        document.getElementById(divnimi).appendChild(uusidiv)
+        lopeta = false
+        i++
+    }
+    else if(i == 9){
+        uusidiv.innerHTML = '<button id="poista" type="submit" onclick="Poista' + formCount + '()">Poista</button>'
         document.getElementById(divnimi).appendChild(uusidiv)
         i++
-        formCount++
-        
     }
     else{
         uusidiv.innerHTML = '<br>'
         document.getElementById(divnimi).appendChild(uusidiv)
         i = 0
+        formCount++
     }
     document.getElementById(divnimi).appendChild(uusidiv)
 }
@@ -72,37 +77,56 @@ function Uudenluominen(divnimi, tyyppi, kysymys, vastaus){
 function Vaihdatilaa(){
     if(title.innerText == "Äänestäminen"){
         document.getElementById('otsikko').innerHTML = "Admintila";
-        button.disabled = true
         Showhidden()
     } else{
         document.getElementById('otsikko').innerHTML = "Äänestäminen";
-        button.disabled = false
         Hide()
     }
     
 }
-function Poista(form){
-    var parentId = document.getElementById('forms');
-    var childName = document.getElementById(form).value;
-    const boxes = Array.from(document.getElementsByClassName(parentId));
-
-    boxes.forEach(box => {
-      box.classList.remove(childName);
-    });
-
-    
+function Poista1(){
+    for(y=0; y < 10; y++){
+        const pois = document.getElementById("form1")
+        pois.remove(pois)
+    }
+}
+function Poista2(){
+    for(y=0; y < 10; y++){
+        const pois = document.getElementById("form2")
+        pois.remove(pois)
+    }
+}
+function Poista3(){
+    for(y=0; y < 10; y++){
+        const pois = document.getElementById("form3")
+        pois.remove(pois)
+    }
+}
+function Poista4(){
+    for(y=0; y < 10; y++){
+        const pois = document.getElementById("form4")
+        pois.remove(pois)
+    }
 }
 
 function Showhidden(){
     var show = document.getElementById("piilota");
     show.style.display = "block";
+    document.getElementById("poista").style.display = "block";
 }
 
 function Hide(){    
     var show = document.getElementById("piilota");
     show.style.display = "none";
+    document.getElementById("poista").style.display = "none";
 }
 
 function Lopeta(){
     lopeta = true
+}
+
+function ClearAndVote(){
+    var ele = document.getElementsByName("myRadioButtons[]");
+    for(var i=0;i<ele.length;i++)
+       ele[i].checked = false;
 }
